6232d594064631fa41b36ff6eff13df9ee499f2c
ezra <ezra54311@gmail.com>
2026-02-02 16:16:51 -0500

    transform module wip- 02-02

---
+++ src/transform_module.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+class DataTransformer:
+
+    def validate(self, df: pd.DataFrame):
+        pass
+
+    def deduplicate(self, df: pd.DataFrame):
+        pass
+
+    def clean(self, df: pd.DataFrame):
+        pass
+
+
