a774c02e0e1b5237afb3cb8b9f9afcfa3212b35d
ericsinkler <ericsinkler2543@gmail.com>
2026-02-09 14:32:48 -0500

    Updated transform module with team stats function to get team stats from season data and updated main pipeline to use new function

--- pipeline_main.py
+++ pipeline_main.py
@@ -4,18 +4,22 @@ from src.extract.extract_module import DataExtractor
 from src.transform.transform_module import DataTransformer
 
 #extract
-extractor = DataExtractor()
 
 file_path = "https://raw.githubusercontent.com/ryurko/nflscrapR-data/refs/heads/master/play_by_play_data/regular_season/reg_pbp_2009.csv"
-test_data = extractor.extract_data(file_path)
+test_data = DataExtractor.extract_data(file_path)
 
 #transform
-transformer = DataTransformer()
 
-valid, rejected = transformer.validate(test_data)
+valid, rejected = DataTransformer.validate(test_data)
 
-cleaned = transformer.clean(valid)
+cleaned = DataTransformer.clean(valid)
 
+team_data = DataTransformer.team_stats(cleaned)
+
+#print(team_data)
+
+print(f"2009 total pass attempts: {sum(team_data['pass_attempts'])}")
+print(f"2009 total rush attempts: {sum(team_data['rush_attempts'])}")
 #load
 
 
--- src/transform/transform_module.py
+++ src/transform/transform_module.py
@@ -11,7 +11,7 @@ rush_touchdown: ER
 """
 class DataTransformer:
 
-    def validate(self, df: pd.DataFrame):
+    def validate(df: pd.DataFrame):
         valid_rows = []
         rejected_rows = []
         for _, row in df.iterrows():
@@ -21,16 +21,33 @@ class DataTransformer:
                 rejected_rows.append(row)
         return pd.DataFrame(valid_rows), rejected_rows
             
-    def clean(self, df: pd.DataFrame):
+    def clean(df: pd.DataFrame):
         
-            df["yards_gained"] = df["yards_gained"].fillna(0).astype(int)
-            df["rush_attempt"] = df["rush_attempt"].fillna(0).astype(int)
-            df["pass_attempt"] = df["pass_attempt"].fillna(0).astype(int)
-            df["touchdown"] = df["touchdown"].fillna(0).astype(int)
-            df["pass_touchdown"] = df["pass_touchdown"].fillna(0).astype(int)
-            df["rush_touchdown"] = df["rush_touchdown"].fillna(0).astype(int)
-            new_df = df[['play_type', 'yards_gained', 'rush_attempt', 'pass_attempt', 'touchdown', 'pass_touchdown', 'rush_touchdown']]
+            df["yards_gained"] = df["yards_gained"].dropna().astype(int)
+            df["rush_attempt"] = df["rush_attempt"].dropna().astype(int)
+            df["pass_attempt"] = df["pass_attempt"].dropna().astype(int)
+            df["touchdown"] = df["touchdown"].dropna().astype(int)
+            df["pass_touchdown"] = df["pass_touchdown"].dropna().astype(int)
+            df["rush_touchdown"] = df["rush_touchdown"].dropna().astype(int)
+            new_df = df[['posteam', 'play_type', 'yards_gained', 'rush_attempt', 'pass_attempt', 'touchdown', 'pass_touchdown', 'rush_touchdown']]
             return new_df
+    
+    def team_stats(season_data):
+        season_data['pass_yards'] = season_data['yards_gained'] * season_data['pass_attempt']
+        season_data['rush_yards'] = season_data['yards_gained'] * season_data['rush_attempt']
+
+        stats = (season_data.groupby('posteam', as_index = False)
+                .agg(
+                    pass_yards = ('pass_yards', 'sum'),
+                    rush_yards = ('rush_yards', 'sum'),
+                    pass_attempts = ('pass_attempt', 'sum'),
+                    rush_attempts = ('rush_attempt', 'sum'),
+                    pass_touchdowns = ('pass_touchdown', 'sum'),
+                    rush_touchdowns = ('rush_touchdown', 'sum'),
+                    )
+                )
+
+        return stats
   
 
 
