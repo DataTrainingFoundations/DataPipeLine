6759659897efad9e140ca88e6691a7d397ff974b
ezra <ezra54311@gmail.com>
2026-02-10 10:19:33 -0500

    sql engine module created. sql engine utilized in load module - ezra 02-10

--- src/load/load_module.py
+++ src/load/load_module.py
@@ -1,23 +1,61 @@
 import pandas as pd
-from src.load.sql_queries_module import sql_insert, sql_update
-from src.load.DBConnection_Module import DbConnector
+from sqlalchemy import text
+from src.db.engine import get_engine
 
 
 class DataLoader:
 
-    def __init__(self):
-        self.db = DbConnector()
+    def map_dtype_to_pg(dtype):
+        if "int" in str(dtype):
+            return "BIGINT"
+        elif "float" in str(dtype):
+            return "DOUBLE PRECISION"
+        elif "bool" in str(dtype):
+            return "BOOLEAN"
+        elif "datetime" in str(dtype):
+            return "TIMESTAMP"
+        else:
+            return "TEXT"
 
-    def load_csv_to_Database(self, df, table_name: str):
+
+    def create_table(self, df: pd.DataFrame, table_name: str, primary_key: str = None):
+
         if df is None or df.empty:
             raise ValueError("DataFrame is empty or None")
-        
-        conn = self.db.connect()
-        cursor = conn.cursor()
-        _insert = sql_queries.sql_insert
-        try:
-            data = pd.read_csv(file_path, dtype=str)
-            return data
-        except Exception as e:
-            print(f"Error loading CSV file: {e}")
-            return None
+
+        columns = []
+        for col, dtype in zip(df.columns, df.dtypes):
+            col_def = f"{col} {self.map_dtype_to_pg(dtype)}"
+            if primary_key and col == primary_key:
+                col_def += " PRIMARY KEY"
+            columns.append(col_def)
+
+        sql = f"""CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)});"""
+
+        with get_engine().begin() as conn:
+            conn.execute(text(sql))
+
+
+    def insert_rows(self, df: pd.DataFrame, table_name: str):
+
+        if df is None or df.empty:
+            raise ValueError("DataFrame is empty or None")
+
+        df.to_sql(
+            table_name,
+            get_engine(),
+            if_exists="append",
+            index=False,
+            method="multi"
+        )
+    
+    def update_rows(self, table_name: str, set_clause: str, condition: str):
+
+        sql = f"""
+        UPDATE {table_name}
+        SET {set_clause}
+        WHERE {condition}
+        """
+
+        with get_engine().begin() as conn:
+            conn.execute(text(sql))
