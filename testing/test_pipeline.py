516cbe119b3105972072637f784e9953839753c2
dmueller34 <dmueller34@gatech.edu>
2026-02-12 13:26:04 -0500

    starting the pytest and laying  the pipelines test

---
+++ testing/test_pipeline.py
@@ -0,0 +1,24 @@
+import pytest
+import pandas
+
+"""Testing the pipelines"""
+
+def test_extract_team():
+    pass
+
+def test_transform_team():
+    pass
+
+def test_load_team():
+    pass
+
+def test_extract_players():
+    pass
+
+def test_transform_players():
+    pass
+
+def test_load_players():
+    pass
+
+
